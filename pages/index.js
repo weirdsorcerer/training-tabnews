b7a35269a7ec420693a970017db44e934db7ec44
weirdsorcerer <161955763+weirdsorcerer@users.noreply.github.com>
2024-05-05 14:48:43 +0000

    german

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 
 function Home() {
-    return <h1>test! JHADHIDHDAIUDAHUIHAUIDAIQWNCNCNOIdoie230923201932903213083</h1>
+    return <h1>German studies</h1>
+	   <h2>Words and Phrases that I learned:</h2>
+	   <h3>hallo -> hello (rálô)</h3>
+	   <h3>ich bin -> i am (iti bim)</h3>
+	   <h3>wie gehts dir? -> how are you? (vee gets dir)</h3>
+	   <h3>mir geths gut -> i am good (meer gets goot)</h3>
+	   <h3>tschuus -> bye (tchuu ss)</h3>
+	   <h3>wilkommen -> welcome (vilcomen)</h3>
+	   <h3>bis spater -> see you later (bish pater)</h3>
+	   <h3>danke -> thank you (danca)</h3>
 }
     export default Home
