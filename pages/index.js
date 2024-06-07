50e5ef0f6adf1408d23a63fb1330f4ffdaf0bebd
weirdsorcerer <161955763+weirdsorcerer@users.noreply.github.com>
2024-06-07 16:08:22 +0000

    coe

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Button Example</title>
-    <style>
-        #displayText {
-            margin-top: 20px;
-            font-size: 20px;
-            color: blue;
-        }
-    </style>
-</head>
-<body>
+function Home() {
+  return (
+    <div>
+      <h1>coe fizin</h1>
+    </div>
+  );
+}
 
-<button id="myButton">clika aki fi</button>
-<div id="displayText"></div>
+function teste() {
+  console.log("indentação errada");
+}
 
-<script>
-    document.getElementById("myButton").addEventListener("click", function() {
-        document.getElementById("displayText").innerText = "coe";
-    });
-</script>
-
-</body>
-</html>
+export default Home;
