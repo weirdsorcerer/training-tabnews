15acbff811f5a83e43d0671c65c67f94c8ff9603
weirdsorcerer <161955763+weirdsorcerer@users.noreply.github.com>
2024-07-19 17:51:32 +0000

    endpoint `status` is finished.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,36 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  //response.status(200).send("OK!");
-  response.status(200).json("são acima da média");
+  const updatedAt = new Date().toISOString(); // controller + model
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseMaxConnectionsRsult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsRsult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;", // forma certa de implementar as queries parametrizadas no node-postgres
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsValues =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+
+    dependencies: {
+      //
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValues,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,18 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http:localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  //console.log(responseby);
+  //expect(responseBody.updated_at).toBeDefined(); // espero o response by vindo da propriedade updated_at esteja definido
+
+  //new Date(responseBody.updated_at).toISOString();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt); // se na propriedade updated_at chegar o valor null não vai ser igual ao unix timestamp
+  //console.log(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  //console.log(responseBody);
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1); // 1 porque se tiver mais de uma pode ter uma conexão vazando
 });
