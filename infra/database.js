9453971df8ac5315765134fd202c5fc5ff743eb4
weirdsorcerer <161955763+weirdsorcerer@users.noreply.github.com>
2024-07-20 13:30:38 +0000

    modified `database.js` file to make `client.connect()` better when it comes to errors

--- infra/database.js
+++ infra/database.js
@@ -8,12 +8,22 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   }); //sync
-  await client.connect(); //async
+
+  console.log('Credenciais do Postgres:"', {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
   try {
+    await client.connect(); //async
     const result = await client.query(queryObject); // async
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end(); // não deixar a conexão pendurada/aberta
   }
